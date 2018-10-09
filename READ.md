b0fd3c5a4ec08b19a1907b4b3fcc7636304e8415
fabricio-pedro <wolfxzer2@gmail.com>
2018-10-08 23:28:36 -0300

    Project initial spring boot jokes app create

---
+++ READ.md
@@ -0,0 +1,3 @@
+# Spring boot jokes App
+
+This Spring Boot web applications display random Chuck Norris Joke on the index.
---
+++ src/main/java/guru/springframework/joke/jokeapp/JokeapppApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.joke.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeapppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeapppApplication.class, args);
+    }
+}
